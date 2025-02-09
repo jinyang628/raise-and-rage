b60e94c3a9e85117d334eaee7a1385ffafa5ee95
jinyang628 <chenjinyang4192@gmail.com>
2025-02-09 12:31:50 -0500

    Update

--- frontend/app/(base)/index.tsx
+++ frontend/app/(base)/index.tsx
@@ -1,42 +1,27 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
 
 import CardWrapper from '@/components/shared/card-wrapper';
 
 export default function MainMenu() {
-  const { user } = useUser();
-
   return (
-    <View className="bg-white">
-      <SignedIn>
-        <CardWrapper>
-          <Text className="my-9 text-center text-5xl font-bold tracking-tight text-white drop-shadow-lg">
-            Raise & Rage
-          </Text>
+    <CardWrapper>
+      <Text className="my-9 text-center text-5xl font-bold tracking-tight text-white drop-shadow-lg">
+        Raise & Rage
+      </Text>
 
-          <Link href="/lobby" asChild>
-            <TouchableOpacity className="mb-6 transform rounded-xl bg-emerald-500 py-4 shadow-xl transition-all hover:bg-emerald-600 active:scale-95 active:bg-emerald-700">
-              <Text className="text-center text-xl font-bold text-white">Play</Text>
-            </TouchableOpacity>
-          </Link>
+      <Link href="/lobby" asChild>
+        <TouchableOpacity className="mb-6 transform rounded-xl bg-emerald-500 py-4 shadow-xl transition-all hover:bg-emerald-600 active:scale-95 active:bg-emerald-700">
+          <Text className="text-center text-xl font-bold text-white">Play</Text>
+        </TouchableOpacity>
+      </Link>
 
-          <Link href="/splitwise" asChild>
-            <TouchableOpacity className="transform rounded-xl bg-rose-500 py-4 shadow-xl transition-all hover:bg-rose-600 active:scale-95 active:bg-rose-700">
-              <Text className="text-center text-xl font-bold text-white">Connect Splitwise</Text>
-            </TouchableOpacity>
-          </Link>
-        </CardWrapper>
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+      <Link href="/splitwise" asChild>
+        <TouchableOpacity className="transform rounded-xl bg-rose-500 py-4 shadow-xl transition-all hover:bg-rose-600 active:scale-95 active:bg-rose-700">
+          <Text className="text-center text-xl font-bold text-white">Connect Splitwise</Text>
+        </TouchableOpacity>
+      </Link>
+    </CardWrapper>
   );
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,16 +2,13 @@ import { useEffect } from 'react';
 import { PaperProvider } from 'react-native-paper';
 import 'react-native-reanimated';
 
-import { tokenCache } from '@/cache';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 
-import ProtectedRoute from '@/components/shared/protected-route';
 
 import '../styles/global.css';
 
@@ -41,20 +38,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        {/* <ProtectedRoute> */}
-        <PaperProvider>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Stack>
-              <Stack.Screen name="(base)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
-            </Stack>
-            <StatusBar style="auto" />
-          </ThemeProvider>
-        </PaperProvider>
-        {/* </ProtectedRoute> */}
-      </ClerkLoaded>
-    </ClerkProvider>
+    <PaperProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(base)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </PaperProvider>
   );
 }
